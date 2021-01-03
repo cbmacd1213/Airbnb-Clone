42dd4bc180ffb84005addb03034124f01b4da100
Colin MacDonald <colinmacdonald1213@yahoo.com>
2021-01-03 00:41:07 -0500

    Search result page

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import {Button} from "@material-ui/core";
+import './SearchPage.css'
+import SearchResult from './SearchResult';
 
 function SearchPage() {
     return (
         <div className="searchPage">
-            <div></div>
+            <div className="searchPage_info">
+                <p>63 stays - 26 August to 30 August - 2 guest</p>
+                <h1>Stays Nearby</h1>
+                <Button variant="outlined">Cancellation Flexibility</Button>
+                <Button variant="outlined">Type of place</Button>
+                <Button variant="outlined">Price</Button>
+                <Button variant="outlined">Rooms and beds</Button>
+                <Button variant="outlined">More filters</Button>
+            </div>
+            <SearchResult
+                img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+                location="Private room in center of London"
+                title="Stay at this spacious Edwardian House"
+                description="1 guest - 1 bedroom - 1 bed - 1.5 shared bathrooms - Wifi - Kitchen - Free parking - Washing Machine"
+                star={4.73}
+                price="$30 / night"
+                total="$117 total"
+            />
         </div>
     )
 }
